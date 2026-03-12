0c487946dbd3003f7ccbce4b3262e829188951fe
safuranaaz34 <safuranaaz34@gmail.com>
2026-03-12 20:11:40 +0530

    initial commit

---
+++ routes/userRoute.js
@@ -0,0 +1,16 @@
+import { getusers,adduser,
+  adduserForm,
+  deleteuser,
+  edituserForm,
+  saveuser } from "../controllers/userController.js";
+import express from "express";
+const userRouter = express.Router();
+
+userRouter.get("/", getusers);
+userRouter.get("/add", adduserForm);
+userRouter.post("/add", adduser);
+userRouter.get("/:id/delete", deleteuser);
+userRouter.get("/:id/edit", edituserForm);
+userRouter.post("/:id/save", saveuser);
+
+export default userRouter;
